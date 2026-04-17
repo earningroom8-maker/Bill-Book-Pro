d1e32834e87446d4cbb740c51d499280e8955bf1
earningroom8-maker <earningroom8@gmail.com>
2026-04-17 05:12:33 +0500

    Update firebase-config.ts

--- src/lib/firebase-config.ts
+++ src/lib/firebase-config.ts
@@ -1,10 +1,9 @@
-export const firebaseConfig = {
-  projectId: "gen-lang-client-0791621593",
-  appId: "1:883423728841:web:95219ebb279b10b66f6918",
-  apiKey: "AIzaSyBScy-Kxt4elE1IfKSf8kEEX9r9w9fUYMs",
-  authDomain: "gen-lang-client-0791621593.firebaseapp.com",
-  firestoreDatabaseId: "ai-studio-04607bfc-8918-4f40-9e77-03c7975dcd8e",
-  storageBucket: "gen-lang-client-0791621593.firebasestorage.app",
-  messagingSenderId: "883423728841",
-  measurementId: ""
+const firebaseConfig = {
+  apiKey: "AIzaSyDzJoKZ2UbAzCjYl-XxACoL6vwCxRYKIiI",
+  authDomain: "bill-book-pro-31b81.firebaseapp.com",
+  projectId: "bill-book-pro-31b81",
+  storageBucket: "bill-book-pro-31b81.firebasestorage.app",
+  messagingSenderId: "1073291650519",
+  appId: "1:1073291650519:web:d7f79afdee47aa2d5cd792",
+  measurementId: "G-6V298H20CL"
 };
